d445d4dab7455615018dd9a6ffcec0f39c989d8e
dimpleparmar042 <dimpleparmar042@gmail.com>
2026-08-17 21:49:47 +0530

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly .express as px
 from streamlit_option_menu import option_menu
 #
 st.set_page_config(page_title='Airlines Flights Dashboard',page_icon='✈️',layout='wide')
